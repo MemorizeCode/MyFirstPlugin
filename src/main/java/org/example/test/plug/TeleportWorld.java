c291d12934138b01fa7447145a707720743448a6
Memorize Code <ilya.bezrukov.2005@mail.ru>
2023-06-28 17:02:07 +0400

    messages :)

--- src/main/java/org/example/test/plug/TeleportWorld.java
+++ src/main/java/org/example/test/plug/TeleportWorld.java
@@ -10,7 +10,6 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.awt.*;
 
 public class TeleportWorld implements CommandExecutor {
     private  final JavaPlugin plugin;
